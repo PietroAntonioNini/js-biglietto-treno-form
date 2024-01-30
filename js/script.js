59ae3347096f37dd508990e797a84c98023ea8e4
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-01-30 21:36:15 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const submitElement = document.querySelector("#submit");
+const resetElement = document.querySelector("#reset");
+
+submitElement.addEventListener("click", 
+    function() {
+        console.log("click del pulsante");
+    }
+)
+
+console.log("dopo il click");
